8c57f0ded211e6441ae66bab651d72d250db5514
justinm-nyc <justin.martinelli@homechef.com>
2022-10-17 12:43:13 -0400

    first commit

---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ApplicationRecord
+    validates :title, presence: true, length: { minimum: 2 }
+    validates :body, presence: true, length: { minimum: 2 }
+
+    belongs_to :user
+end
---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+    validates :name, presence: true, length: { minimum: 2 }
+    validates :email, presence: true, length: { minimum: 6 }, uniqueness: true
+    validates :username, presence: true, length: { minimum: 6 }, uniqueness: true
+    validates :password, presence: true, length: { in: 6..20 }
+
+    has_many :posts
+end
